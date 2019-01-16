4245b7ef6f63ce3bcf3d463fb1eece42147f2cdd
dev-iskan <successfuliskan@gmail.com>
2019-01-16 18:26:53 +0500

    adde email notifications when chages statuses of files

--- app/Http/Controllers/Admin/FileNewController.php
+++ app/Http/Controllers/Admin/FileNewController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\File;
 use App\Http\Controllers\Controller;
+use App\Mail\Files\FileApproved;
+use App\Mail\Files\FileRejected;
+use Illuminate\Support\Facades\Mail;
 
 class FileNewController extends Controller
 {
@@ -15,12 +18,15 @@ class FileNewController extends Controller
     public function update (File $file) {
 //        $this->authorize()
         $file->approve();
+        //send email
+        Mail::to($file->user)->send(new FileApproved($file));
         return back()->withSuccess("{$file->title} has been approved");
     }
 
     public function destroy (File $file) {
         $file->delete();
         $file->uploads->each->delete();
+        Mail::to($file->user)->send(new FileRejected($file));
         return back()->withSuccess("{$file->title} has been rejected");
     }
 }
--- app/Http/Controllers/Admin/FileUpdatedController.php
+++ app/Http/Controllers/Admin/FileUpdatedController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\File;
 use App\Http\Controllers\Controller;
+use App\Mail\Files\FileUpdatesApproved;
+use App\Mail\Files\FileUpdatesRejected;
+use Illuminate\Support\Facades\Mail;
 
 class FileUpdatedController extends Controller
 {
@@ -19,12 +22,14 @@ class FileUpdatedController extends Controller
         $file->approveAllUploads();
         // delete all approvals
         $file->deleteAllApprovals();
+        Mail::to($file->user)->send(new FileUpdatesApproved($file));
         return back()->withSuccess("{$file->title} changes have been approved");
     }
 
     public function destroy (File $file) {
         $file->deleteAllApprovals();
         $file->deleteUnapprovedUploads();
+        Mail::to($file->user)->send(new FileUpdatesRejected($file));
         return back()->withSuccess("{$file->title} changes have been rejected");
     }
 }
